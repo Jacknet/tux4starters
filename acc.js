091718866102babbef37831d17fb2c82c0530135
aherrera3usf <aherrera3@usf.edu>
2021-04-08 16:12:12 -0400

    Latest for accordion js

--- acc.js
+++ acc.js
@@ -1,9 +1,9 @@
-var acc = document.querySelectorAll(".less-acc, .par-acc");
+var acc = document.querySelectorAll("less-acc, par-acc");
 var i;
 
 for (i = 0; i < acc.length; i++) {
-    acc[i].addEventListener("click", function() {
-        this.classList.toggle("active");
+    acc[i].addEventListener("click", function(){
+        this.classList.toggle('active');
         var panel= this.nextElementSibling;
             if (panel.style.display === "block") {
                 panel.style.display = "none";
@@ -17,7 +17,13 @@ for (i = 0; i < acc.length; i++) {
     acc[i].onkeypress = function(key) {
         if (key.keyCode == 0 || key.keycode == 32) {
             this.classList.toggle("active");
-            this.nextElementSibling.classList.toggle("show"); return false;
+            var panel= this.nextElementSibling;
+            if (panel.style.display === "block") {
+                panel.style.display = "none";
+            }
+            else {
+                panel.style.display = "block";
+            }
         }
     }
     
