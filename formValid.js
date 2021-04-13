10d0edd15974dfa55d56b498ed5c525aa11e3962
JustinAsbury <justinasbury@usf.edu>
2021-04-12 21:35:24 -0400

    Working Password requirements

--- formValid.js
+++ formValid.js
@@ -1,22 +1,52 @@
-const username = document.getElementById('username')
-const email = document.getElementById('email')
-const password = document.getElementById('password')
-const form = document.getElementById('form')
-const errorElement = document.getElementById('error')
+<script>
+var myInput = document.getElementById("password");
+var letter = document.getElementById("letter");
+var capital = document.getElementById("capital");
+var number = document.getElementById("number");
+var length = document.getElementById("length");
 
-form.addEventListener('submit', (e) => {
- 
-    let messages = []
+myInput.onfocus = function() {
+  document.getElementById("message").style.display = "block";
+}
 
-    if (password.value.length < 8){
-        messages.push('Password must have at least 8 characters')
-    }
-    if (password.value.length > 16){
-        messages.push('Password must be less than 16 characters')
-    }
+myInput.onblur = function() {
+  document.getElementById("message").style.display = "none";
+}
 
-    if (messages.length > 0){
-        e.preventDefault()
-        errorElement.innerText = messages.join(', ')
-    }
-});
+myInput.onkeyup = function() {
+  var lowerCaseLetters = /[a-z]/g;
+  if(myInput.value.match(lowerCaseLetters)) {
+    letter.classList.remove("invalid");
+    letter.classList.add("valid");
+  } else {
+    letter.classList.remove("valid");
+    letter.classList.add("invalid");
+  }
+
+  var upperCaseLetters = /[A-Z]/g;
+  if(myInput.value.match(upperCaseLetters)) {
+    capital.classList.remove("invalid");
+    capital.classList.add("valid");
+  } else {
+    capital.classList.remove("valid");
+    capital.classList.add("invalid");
+  }
+
+  var numbers = /[0-9]/g;
+  if(myInput.value.match(numbers)) {
+    number.classList.remove("invalid");
+    number.classList.add("valid");
+  } else {
+    number.classList.remove("valid");
+    number.classList.add("invalid");
+  }
+
+  if(myInput.value.length >= 8) {
+    length.classList.remove("invalid");
+    length.classList.add("valid");
+  } else {
+    length.classList.remove("valid");
+    length.classList.add("invalid");
+  }
+}
+</script>
